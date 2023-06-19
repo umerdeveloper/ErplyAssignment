0944ff37765f4de0d32166040efb7b48d4067f79
Umer Khan <umer.khan@lab.mobi>
2023-06-19 21:24:48 +0300

    initial setup

---
+++ ErplyAssignment/ErplyAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErplyAssignmentApp.swift
+//  ErplyAssignment
+//
+//  Created by Umer Khan on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ErplyAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
