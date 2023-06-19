a179cd46028b280c169067ed9846368f2525f078
Umer Khan <umer.khan@lab.mobi>
2023-06-19 22:59:31 +0300

    use focusState for textField

--- ErplyAssignment/Screens/Login/LoginScreen.swift
+++ ErplyAssignment/Screens/Login/LoginScreen.swift
@@ -7,23 +7,40 @@
 
 import SwiftUI
 
+enum Field: Hashable {
+    case email
+}
+
 struct LoginScreen: View {
 
     @StateObject var viewModel = LoginViewModel()
+    @FocusState private var focusedField: Field?
 
     var body: some View {
         VStack(alignment: .center, spacing: 20) {
             TextField("Email", text: $viewModel.email)
                 .padding()
                 .textFieldStyle(.roundedBorder)
+                .keyboardType(.emailAddress)
+                .focused($focusedField, equals: .email)
             
             Button {
+                focusedField = nil
                 viewModel.perform(action: .handleLogin)
             } label: {
                 Text("Login")
             }
             .buttonStyle(.borderedProminent)
             .disabled(viewModel.isValidEmail == false)
+            
+            Spacer()
+        }
+        .onAppear {
+            focusedField = .email
+        }
+        .contentShape(Rectangle()) // Otherwise tap gesture doesn't work on spaces or blank white areas
+        .onTapGesture {
+            focusedField = nil
         }
         .navigationTitle("Login")
     }
